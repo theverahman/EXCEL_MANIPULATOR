410a83aecc61c02e73eddff339ac422eae83f24f
theverahman <43458924+theverahman@users.noreply.github.com>
2025-01-24 01:14:58 +0700

    testing mastering related method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Irony;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -1492,7 +1493,8 @@ namespace LIBEXCELMANIPULATOR
                         for (int ivy = 0; ivy < (_cellRsideMasterStep2VarMap[iv].RowCount() - 1); ivy++)
                         {
                             b = _cellRsideMasterStep2VarMap[iv].Cell(ivy + 1, 1).TryGetValue<double>(out debuff);
-                            scope.Add(Convert.ToSingle(debuff));  
+                            scope.Add(Convert.ToSingle(debuff));
+                            Debug.WriteLine(Convert.ToSingle(debuff));
                         }
                         buffer.Add(scope);
                     }
